a77ad68f58de101cf375e332f56227d977611a26
Icebird-algo <jokermanch@gmail.com>
2023-04-19 22:38:37 +0200

    Content update

--- icebird-prelanding/src/components/page-content.tsx
+++ icebird-prelanding/src/components/page-content.tsx
@@ -2,9 +2,19 @@ import { Grid } from "@mui/material";
 
 export default function PageContent () {
   return(
-    <Grid container>
-      <Grid item xs={6}>asd</Grid>
-      <Grid item xs={6}>asd</Grid>
-    </Grid>
+    <>
+      <Grid container>
+        <Grid item xs={6}>
+          <div>Icebird Algo</div>
+          <hr/>
+          <div>For You</div>
+        </Grid>
+        <Grid item xs={6}>LOGÓ</Grid>
+      </Grid>
+      <Grid container>
+        <Grid item xs={6}>KÉP A</Grid>
+        <Grid item xs={6}>KÉP B</Grid>
+      </Grid>
+    </>
   )
 }
